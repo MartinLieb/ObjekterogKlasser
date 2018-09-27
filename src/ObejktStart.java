7f5b0f87ec3f2b4fc7b96aa3992820acf1636531
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-09-27 10:47:49 +0200

    Initial commit

---
+++ src/ObejktStart.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ObejktStart {
+
+    public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+        System.out.println("indtast navn?");
+
+    String navn = input.nextLine();
+
+        System.out.println(navn);
+
+
+    }
+
+
+}
